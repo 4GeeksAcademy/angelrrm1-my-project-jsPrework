a1144e1d5e3e7ba63989b1468d99eaaad86dffaf
angelrrm1 <angelrramosm@icloud.com>
2024-11-29 14:21:35 +0000

    funny-Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = {
+    who: ["The dog", "My human grandma ", "The mailman", "My bird"],
+    action: ["ate", "peed", "crushed", "broke"],
+    what: ["my homework", "my phone", "the car"],
+    when: [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ]
+  };
+  function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  function generateExcuse() {
+    const { who, action, what, when } = excuse;
+    return [
+      randomElement(who),
+      randomElement(action),
+      randomElement(what),
+      randomElement(when)
+    ].join(" ");
+  }
+  const excuseElement = document.getElementById("excuse");
+  if (excuseElement) {
+    excuseElement.innerHTML = generateExcuse();
+  }
 };
